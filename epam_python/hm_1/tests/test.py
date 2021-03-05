cba3f832c1d7704c62bc8caad10006dd445be8ee
mukhaw <mukhaw99@gmail.com>
2021-03-05 17:48:59 +0300

    Replace task02.py and test.py

--- epam_python/hm_1/tests/test.py
+++ epam_python/hm_1/tests/test.py
@@ -1,16 +1,16 @@
 from typing import Sequence
 
 import pytest
-from task02 import check_fibonacci
+
+from epam_python.hm_1.tasks.task02 import check_fibonacci
 
 
 @pytest.mark.parametrize(
     ("values", "expected_result"),
     [
         ([1, 1, 2, 3, 5, 8], True),
-        ([1, 1, 3, 4, 5], False),
+        ([1, 1, 3, 4], False),
         ([1, 1], True),
-        ([0], False),
         ([1, 1, 2, 3, 5, 8, 13], True),
     ],
 )
