887accb6e684115a75780107ccc2b940359cdd8b
mukhaw <mukhaw99@gmail.com>
2021-03-09 14:18:58 +0300

    Fix homework 01 task 02 and test 02

---
+++ test/test_01_02.py
@@ -0,0 +1,25 @@
+from typing import Sequence
+
+import pytest
+
+from hm_1.tasks.task_01_02 import check_fibonacci
+
+
+@pytest.mark.parametrize(
+    "value",
+    [[0, 1, 1, 2], [0, 1, 1]],
+)
+def test_sequence_is_fibonacci(value: Sequence[int]):
+    assert check_fibonacci(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [[0], [4], [0, 1, 1, 3], [1, 1, 2], [1, 1]],
+)
+def test_sequence_is_not_fibonacci(value: Sequence[int]):
+    assert check_fibonacci(value) is False
+
+
+def test_empty_sequence_is_fibonacci():
+    assert check_fibonacci([]) is False
