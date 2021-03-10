99d4195f22ca8a0899afba1cd5e27bca3fb3857a
mukhaw <mukhaw99@gmail.com>
2021-03-10 19:09:39 +0300

    Fix cycle in homework 01 task 02 and add some new cases in test 02

---
+++ hm_1/tasks/task_01_02.py
@@ -0,0 +1,18 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+    please write function "check_fib", which accepts a Sequence of integers, and
+    returns if the given sequence is a Fibonacci sequence
+We guarantee, that the given sequence contain >= 0 integers inside.
+"""
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    res = 0
+    if len(data) <= 1 or (data[0:2] != [0, 1]):
+        res = -1
+    else:
+        for i in range(len(data) - 2):
+            if data[i + 2] != sum(data[i : i + 2]):
+                res = -1
+    return False if res == -1 else True
