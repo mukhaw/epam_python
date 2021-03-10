0bbc43105db086f5b0db25a2d5e1ccf968b30dee
mukhaw <mukhaw99@gmail.com>
2021-03-11 01:05:14 +0300

    In homework 01 task 05 change the loop while to for and delete case with empty list In homework 01 test 05 delete case with empty list

---
+++ hm_1/tasks/task_01_05.py
@@ -0,0 +1,17 @@
+"""
+Given a list of integers numbers "nums".
+You need to find a sub-array with length less equal to "k", with maximal sum.
+The written function should return the sum of this sub-array.
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+from typing import List
+
+
+def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+    sums = []
+    for j in range(k, -1, -1):
+        for i in range(len(nums) - (j - 1)):
+            sums.append(sum(nums[i : i + j]))
+    return "Size of subarray > length of your list" if k > len(nums) else max(sums)
