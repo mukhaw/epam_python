11ed41e920450aff71bdc3b96ff63cf586f8402d
mukhaw <mukhaw99@gmail.com>
2021-03-10 23:27:53 +0300

    Add more cases in homework 01 test 01 (in test 01 I read that sqrt(0) == 0 and I think the function must be return True for this case

---
+++ test/test_01_01.py
@@ -0,0 +1,16 @@
+import pytest
+
+from hm_1.tasks.task_01_01 import check_power_of_2
+
+
+@pytest.mark.parametrize("value", [0, 1, 4, 9, 16, 64, 144, 65536])
+def test_power_of_two_returns_true(value: int):
+    assert check_power_of_2(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [2, 56, 678, 1000],
+)
+def test_power_of_two_returns_false(value: int):
+    assert check_power_of_2(value) is False
