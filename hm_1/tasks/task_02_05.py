6273fccc8568ed63afa866c80c522e9c67a94ffa
mukhaw <mukhaw99@gmail.com>
2021-03-16 19:54:04 +0300

    Add homework 02 task 05 and test 05

--- hm_1/tasks/task_02_05.py
+++ hm_1/tasks/task_02_05.py
@@ -1,4 +1,25 @@
-def custom_range(some_iterable_object, start=1, stop=1, step=1):
-    start = some_iterable_object.index(start)
-    stop = some_iterable_object.index(stop)
-    return list(some_iterable_object[start:stop:step])
+def detect_index(some_iterable_object, position):
+    if isinstance(some_iterable_object, dict):
+        if position in list(some_iterable_object.keys()):
+            return list(some_iterable_object.keys()).index(position)
+        else:
+            return list(some_iterable_object.values()).index(position)
+    return some_iterable_object.index(position)
+
+
+def custom_range(some_iterable_object, start="", stop="", step=1):
+    if not some_iterable_object:
+        return "Wrong values"
+    start = 0 if start == "" else detect_index(some_iterable_object, start)
+    stop = (
+        len(some_iterable_object)
+        if stop == ""
+        else detect_index(some_iterable_object, stop)
+    )
+    if isinstance(some_iterable_object, list) or isinstance(
+        some_iterable_object, tuple
+    ):
+        return some_iterable_object[start:stop:step]
+    if isinstance(some_iterable_object, dict):
+        return list(some_iterable_object.items())[start:stop:step]
+    return list(some_iterable_object)[start:stop:step]
