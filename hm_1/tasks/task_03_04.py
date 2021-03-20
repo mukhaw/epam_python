f385d32870d4c348714ce34aeee002391f3a9a69
mukhaw <mukhaw99@gmail.com>
2021-03-20 15:20:06 +0300

    Add homework 03 task 04 and test 04

---
+++ hm_1/tasks/task_03_04.py
@@ -0,0 +1,19 @@
+"""
+Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+https://en.wikipedia.org/wiki/Narcissistic_number
+Examples:
+- 9 is an Armstrong number, 9 = 9^1 = 9
+- 10 is not: 10 != 1^2 + 0^2 = 1
+- 153 is : 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+Write a function that detects if a number is Armstrong number in functionaly style:
+ - use map or other utilities from functools library,
+ - use anonymous functions (or use function as argument)
+ - do not use loops, preferably using list comprehensions
+### Example function signature and call
+"""
+
+
+def is_armstrong(number: int) -> bool:
+    numbers = [int(i) for i in str(number)]
+    sum_of_digits = sum((number ** len(numbers) for number in numbers))
+    return number == sum_of_digits and number != 0
---
+++ test/test_03_04.py
@@ -0,0 +1,16 @@
+import pytest
+
+from hm_1.tasks.task_03_04 import is_armstrong
+
+
+@pytest.mark.parametrize("value", [1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+def test_power_of_two_returns_true(value: int):
+    assert is_armstrong(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [0, 31, 511, 1023],
+)
+def test_power_of_two_returns_false(value: int):
+    assert is_armstrong(value) is False
