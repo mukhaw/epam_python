b31f4cba0956506544ecf3ae4c3c5a14d71f9d30
mukhaw <mukhaw99@gmail.com>
2021-03-20 21:14:27 +0300

    Fix  Homework 03 test 04

--- test/test_03_04.py
+++ test/test_03_04.py
@@ -4,7 +4,7 @@ from hm_1.tasks.task_03_04 import is_armstrong
 
 
 @pytest.mark.parametrize("value", [1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
-def test_power_of_two_returns_true(value: int):
+def test_is_armstrong_returns_true(value: int):
     assert is_armstrong(value) is True
 
 
@@ -12,5 +12,5 @@ def test_power_of_two_returns_true(value: int):
     "value",
     [0, 31, 511, 1023],
 )
-def test_power_of_two_returns_false(value: int):
+def test_is_armstrong_returns_false(value: int):
     assert is_armstrong(value) is False
