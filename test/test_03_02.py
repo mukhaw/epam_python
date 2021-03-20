9443f78be4c2b3c0d0da41c4da55a914bdec43d6
mukhaw <mukhaw99@gmail.com>
2021-03-20 22:37:46 +0300

    Fix homework 03 task 02 and test 02

---
+++ test/test_03_02.py
@@ -0,0 +1,16 @@
+import pytest
+
+from hm_1.tasks.task_03_02 import parallel_load
+
+
+@pytest.mark.parametrize("value", [25, 50, 75, 100, 200, 300, 400, 500])
+def test_power_of_two_returns_true(value: int):
+    assert parallel_load(value) is True
+
+
+@pytest.mark.parametrize(
+    "value",
+    [10, 20],
+)
+def test_power_of_two_returns_false(value: int):
+    assert parallel_load(value) is False
