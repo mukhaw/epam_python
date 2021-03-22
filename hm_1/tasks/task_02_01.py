1c2bbcf8c7bebcc5082d63258e80d068f79ad6dc
mukhaw <mukhaw99@gmail.com>
2021-03-22 21:43:59 +0300

    Fix homework 02 task 01 and test 01

--- hm_1/tasks/task_02_01.py
+++ hm_1/tasks/task_02_01.py
@@ -27,10 +27,10 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
     words = text_without_punctuation.split()
     for i in words:
         data[i] = len(set(i.lower()))
-    k = len(data) if len(data) < 10 else 10
+    size_of_data = len(data) if len(data) < 10 else 10
     result = sorted(data.items(), key=lambda x: x[1], reverse=True)
     list_10_longest_words = []
-    for i in sorted(result[0:k], key=lambda x: len(x[0]), reverse=True):
+    for i in sorted(result[0:size_of_data], key=lambda x: len(x[0]), reverse=True):
         list_10_longest_words.append(i[0])
     return list_10_longest_words
 
@@ -55,9 +55,11 @@ def count_non_ascii_chars(file_path: str) -> int:
 
 def get_most_common_non_ascii_char(file_path: str) -> str:
     text = get_text(file_path)
-    data = {}
-    for i in text.lower():
-        if not i.isascii():
-            data[i] = text.count(i)
+    text_without_non_ascii_chars = text.encode("ascii", "ignore").decode()
+    non_ascii_chars = ""
+    for i in text:
+        if i not in text_without_non_ascii_chars:
+            non_ascii_chars += i
+    data = Counter(non_ascii_chars)
     key, _ = max(data.items(), key=lambda x: x[1])
     return key
