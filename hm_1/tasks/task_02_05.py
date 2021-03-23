38d23a9066fb27b02065f1822a89f2bd51e7ca92
mukhaw <mukhaw99@gmail.com>
2021-03-23 16:19:01 +0300

    Fix homework 02 task 05 and test 05

--- hm_1/tasks/task_02_05.py
+++ hm_1/tasks/task_02_05.py
@@ -1,33 +1,24 @@
 def detect_index(some_iterable_object, position):
-    if isinstance(some_iterable_object, dict):
-        if position in list(some_iterable_object.keys()):
-            return list(some_iterable_object.keys()).index(position)
-        else:
-            return list(some_iterable_object.values()).index(position)
     return some_iterable_object.index(position)
 
 
 def detect_stop_start_step(some_iterable_object, args):
     if not args:
-        args = 0, len(some_iterable_object), 1
-        return args
+        return 0, len(some_iterable_object), 1
     elif len(args) == 1:
-        args = 0, detect_index(some_iterable_object, args[0]), 1
-        return args
+        return 0, detect_index(some_iterable_object, args[0]), 1
     elif len(args) == 2:
-        args = (
+        return (
             detect_index(some_iterable_object, args[0]),
             detect_index(some_iterable_object, args[1]),
             1,
         )
-        return args
     elif len(args) == 3:
-        args = (
+        return (
             detect_index(some_iterable_object, args[0]),
             detect_index(some_iterable_object, args[1]),
             args[2],
         )
-        return args
     else:
         return "Wrong values"
 
@@ -40,6 +31,4 @@ def custom_range(some_iterable_object, *args):
         some_iterable_object, tuple
     ):
         return some_iterable_object[start:stop:step]
-    if isinstance(some_iterable_object, dict):
-        return list(some_iterable_object.items())[start:stop:step]
     return list(some_iterable_object)[start:stop:step]
