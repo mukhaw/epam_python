b19f5801ae55b93d6d9debf15faa68f590522347
mukhaw <mukhaw99@gmail.com>
2021-03-26 10:59:11 +0300

    Fix homework 04 task 01 and test 01

--- hm_1/tasks/task_04_01.py
+++ hm_1/tasks/task_04_01.py
@@ -3,5 +3,5 @@ def read_magic_number(path: str) -> bool:
         with open(path) as f:
             file = f.readline().rstrip("\n")
         return file.isdigit() and int(file) >= 1 and int(file) < 3
-    except ValueError as err:
-        return err
+    except Exception:
+        return ValueError
--- test/test_04_01.py
+++ test/test_04_01.py
@@ -15,3 +15,21 @@ def test_read_magic_number_false_():
     d.write("test.txt", b"3")
     assert read_magic_number(d.path + "/test.txt") is False
     d.cleanup()
+
+
+def test_read_magic_number_return_true():
+    d = TempDirectory()
+    d.write("test.txt", b"1")
+    assert read_magic_number(d.path + "/test.txt") is True
+    d.cleanup()
+
+
+def test_read_magic_number_return_true_():
+    d = TempDirectory()
+    d.write("test.txt", b"2")
+    assert read_magic_number(d.path + "/test.txt") is True
+    d.cleanup()
+
+
+def test_read_magic_number_return_value_error():
+    assert read_magic_number("/test.txt") is ValueError
