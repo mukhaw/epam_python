1616fe4e92b23a51f7599d67c008ddb053189b6f
mukhaw <mukhaw99@gmail.com>
2021-03-29 21:09:23 +0300

    Fix homework 03 task 02 and test 02

--- hm_1/tasks/task_03_02.py
+++ hm_1/tasks/task_03_02.py
@@ -31,4 +31,4 @@ def slow_calculate(value):
 
 def parallel_load(flow: int):
     with Pool(flow) as p:
-        p.map(slow_calculate, range(0, 500))
+        sum(p.map(slow_calculate, range(0, 500)))
