0ff3ae810bcf2651be6195926cf99c10d7d6232d
mukhaw <mukhaw99@gmail.com>
2021-03-29 21:21:01 +0300

    Fix homework 03 task 02 and test 02

--- test/test_03_02.py
+++ test/test_03_02.py
@@ -6,7 +6,7 @@ from hm_1.tasks.task_03_02 import parallel_load
 
 
 @pytest.mark.parametrize("value", [25, 50, 75, 100, 200, 300, 400, 500])
-def test_power_of_two_returns_true(value: int):
+def test_parallel_load_works_less_than_a_minute(value: int):
     start_time = time.time()
     parallel_load(value)
     assert bool((time.time() - start_time) <= 60.0) is True
@@ -16,7 +16,7 @@ def test_power_of_two_returns_true(value: int):
     "value",
     [10],
 )
-def test_power_of_two_returns_false(value: int):
+def test_parallel_load_works_more_than_a_minute(value: int):
     start_time = time.time()
     parallel_load(value)
 
