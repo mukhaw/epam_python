57752c2992aa8204ec7ac6a65fae2680aa4c7a2a
mukhaw <mukhaw99@gmail.com>
2021-04-01 22:43:36 +0300

    Add homework 03 task 03 and test 03

---
+++ hm_1/tasks/task_03_03.py
@@ -0,0 +1,43 @@
+# I decided to write a code that generates data filtering object from a list of keyword parameters:
+# example of usage:
+# positive_even = Filter(lamba a: a % 2 == 0, lambda a: a > 0, lambda a: isinstance(int, a)))
+# positive_even.apply(range(100)) should return only even numbers from 0 to 99
+# make_filter(name='polly', type='bird').apply(sample_data) should return only second entry from the list
+
+# There are multiple bugs in this code. Find them all and write tests for faulty cases.
+
+
+class Filter:
+    """
+    Helper filter class. Accepts a list of single-argument
+    functions that return True if object in list conforms to some criteria
+    """
+
+    def __init__(self, *functions):
+        self.functions = functions
+
+    def apply(self, data):
+        result = [item for item in data if all(i(item) for i in self.functions)]
+        return "It is faulty case" if not result else result
+
+
+def make_filter(**keywords):
+
+    filter_funcs = []
+    for key, value in keywords.items():
+        filter_funcs.append(lambda x: x[key] == value)
+    if not filter_funcs:
+        return "It is faulty case"
+    return Filter(*filter_funcs)
+
+
+sample_data = [
+    {
+        "name": "Bill",
+        "last_name": "Gilbert",
+        "occupation": "was here",
+        "type": "person",
+    },
+    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
+    {"is_dead": False, "kind": "swallow", "type": "bird", "name": "polly"},
+]
---
+++ test/test_03_03.py
@@ -0,0 +1,41 @@
+import pytest
+
+from hm_1.tasks.task_03_03 import Filter, make_filter, sample_data
+
+
+@pytest.mark.parametrize(
+    ("function", "data", "exepected_result"),
+    [
+        ((lambda a: a % 3 == 0, lambda a: a > 3), range(0, 20), [6, 9, 12, 15, 18]),
+        (
+            (lambda a: a & (a - 1) == 0 and a != 0, lambda a: a <= 16),
+            range(0, 64),
+            [1, 2, 4, 8, 16],
+        ),
+    ],
+)
+def test_filter_apply_returns_filtered_list(function, data, exepected_result):
+    assert Filter(*function).apply(data) == exepected_result
+
+
+def test_filter_apply_with_faulty_cases_returns_message_about_it():
+    assert Filter(lambda a: a > 0).apply(range(-5, 0)) == "It is faulty case"
+
+
+def test_filter_apply_with_empty_case_returns_data_for_confition():
+    assert Filter().apply(range(-5, 0)) == [-5, -4, -3, -2, -1]
+
+
+def test_make_filter_returns_data_with_name_and_type_parameters():
+    exepected_result = [
+        {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
+        {"is_dead": False, "kind": "swallow", "type": "bird", "name": "polly"},
+    ]
+
+    assert make_filter(name="polly", type="bird").apply(sample_data) == exepected_result
+
+
+def test_make_filter_with_faulty_case_returns_message_about_it():
+    assert (
+        make_filter(name="polly", type="dog").apply(sample_data) == "It is faulty case"
+    )
