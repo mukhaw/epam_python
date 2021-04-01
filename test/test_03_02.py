cb6f5af8af4c9027c4ed4d35acddbda29c57bffe
mukhaw <mukhaw99@gmail.com>
2021-04-01 16:14:55 +0300

    Fix homework 03 test 02

--- test/test_03_02.py
+++ test/test_03_02.py
@@ -4,12 +4,17 @@ import pytest
 
 from hm_1.tasks.task_03_02 import parallel_load
 
+exepected_result = 1024259
+
 
 @pytest.mark.parametrize("value", [25, 50, 75, 100, 200, 300, 400, 500])
 def test_power_of_two_returns_true(value: int):
     start_time = time.time()
-    parallel_load(value)
-    assert bool((time.time() - start_time) <= 60.0) is True
+    actual_result = parallel_load(value)
+    assert (
+        bool((time.time() - start_time) <= 60.0 and actual_result == exepected_result)
+        is True
+    )
 
 
 @pytest.mark.parametrize(
@@ -18,6 +23,8 @@ def test_power_of_two_returns_true(value: int):
 )
 def test_power_of_two_returns_false(value: int):
     start_time = time.time()
-    parallel_load(value)
-
-    assert bool((time.time() - start_time) <= 60.0) is False
+    actual_result = parallel_load(value)
+    assert (
+        bool((time.time() - start_time) <= 60.0 and actual_result == exepected_result)
+        is False
+    )
