57c24fe73c69bf263e81a485b83fe84af2da6fcf
mukhaw <mukhaw99@gmail.com>
2021-04-03 15:17:12 +0300

    Modify homework 04 test 03

--- test/test_04_03.py
+++ test/test_04_03.py
@@ -1,6 +1,11 @@
 from hm_1.tasks.task_04_03 import my_precious_logger
 
 
-def test_my_precious_logger():
+def test_my_precious_logger_returns_error(capsys):
     my_precious_logger("error: file not found")
-    assert my_precious_logger("error: file not found") == "error: file not found"
+    assert capsys.readouterr().err == "error: file not found"
+
+
+def test_my_precious_logger_returns_ok(capsys):
+    my_precious_logger("ok")
+    assert capsys.readouterr().out == "ok"
