bc24f553313be9159237c0954503ce270e2d3072
mukhaw <mukhaw99@gmail.com>
2021-04-04 13:26:38 +0300

    Fix homework 04 tset 02

--- test/test_04_02.py
+++ test/test_04_02.py
@@ -1,5 +1,10 @@
-from unittest.mock import Mock
+from unittest.mock import patch
 
 from hm_1.tasks.task_04_02 import count_dots_on_i
 
-mock = Mock(count_dots_on_i())
+
+def test_count_dots_on_i():
+    with patch("requests.get") as mock_request:
+        url = "https://docs.python.org/3/library/urllib.request.html#urllib.request.urlopen"
+        mock_request.return_value = 7019
+        assert count_dots_on_i(url) == mock_request.return_value
