39b8df2376426d4433fcf8c17911d71295d84a8f
mukhaw <mukhaw99@gmail.com>
2021-04-06 23:01:30 +0300

    Fix homework 04 task 01 and test 01

--- hm_1/tasks/task_04_01.py
+++ hm_1/tasks/task_04_01.py
@@ -1,7 +1,10 @@
 def read_magic_number(path: str) -> bool:
     try:
         with open(path) as file:
-            number = int(file.read().rstrip("\n"))
-            return number >= 1 and number < 3
+            line = file.read().rstrip("\n")
+            if line.isdigit():
+                return int(line) >= 1 and int(line) < 3
+            else:
+                return False
     except Exception:
         raise ValueError("This is ValueError mistake")
--- test/test_04_01.py
+++ test/test_04_01.py
@@ -6,16 +6,40 @@ from hm_1.tasks.task_04_01 import read_magic_number
 
 
 def test_read_magic_number_returns_true():
-    f = NamedTemporaryFile(delete=False)
+    f = NamedTemporaryFile(delete=True)
     f.write(b"1\n")
     f.seek(0)
     assert read_magic_number(f.name) is True
     f.close()
 
 
-def test_read_magic_number_returns_false():
-    f = NamedTemporaryFile(delete=False)
+def test_read_magic_number_returns_true_():
+    f = NamedTemporaryFile(delete=True)
+    f.write(b"2\n")
+    f.seek(0)
+    assert read_magic_number(f.name) is True
+    f.close()
+
+
+def test_read_magic_number_with_wrong_number_returns_false():
+    f = NamedTemporaryFile(delete=True)
     f.write(b"3")
+    f.seek(0)
+    assert read_magic_number(f.name) is False
+    f.close()
+
+
+def test_read_magic_number_with_str_returns_false():
+    f = NamedTemporaryFile(delete=True)
+    f.write(b"kljl")
+    f.seek(0)
+    assert read_magic_number(f.name) is False
+    f.close()
+
+
+def test_read_magic_number_with_empty_str_returns_false():
+    f = NamedTemporaryFile(delete=True)
+    f.write(b"")
     f.seek(0)
     assert read_magic_number(f.name) is False
     f.close()
