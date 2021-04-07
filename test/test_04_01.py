a8e3ef6d1997333a60ec2e01113dfa2583b22b5a
mukhaw <mukhaw99@gmail.com>
2021-04-07 23:00:08 +0300

    Fix cleanup in homework 04 test 01

--- test/test_04_01.py
+++ test/test_04_01.py
@@ -9,40 +9,45 @@ def test_read_magic_number_returns_true():
     f = NamedTemporaryFile(delete=True)
     f.write(b"1\n")
     f.seek(0)
-    assert read_magic_number(f.name) is True
+    actual_result = read_magic_number(f.name)
     f.close()
+    assert actual_result is True
 
 
 def test_read_magic_number_returns_true_():
     f = NamedTemporaryFile(delete=True)
     f.write(b"2\n")
     f.seek(0)
-    assert read_magic_number(f.name) is True
+    actual_result = read_magic_number(f.name)
     f.close()
+    assert actual_result is True
 
 
 def test_read_magic_number_with_wrong_number_returns_false():
     f = NamedTemporaryFile(delete=True)
     f.write(b"3")
     f.seek(0)
-    assert read_magic_number(f.name) is False
+    actual_result = read_magic_number(f.name)
     f.close()
+    assert actual_result is False
 
 
 def test_read_magic_number_with_str_returns_false():
     f = NamedTemporaryFile(delete=True)
-    f.write(b"kljl")
+    f.write(b"hkjh\n")
     f.seek(0)
-    assert read_magic_number(f.name) is False
+    actual_result = read_magic_number(f.name)
     f.close()
+    assert actual_result is False
 
 
 def test_read_magic_number_with_empty_str_returns_false():
     f = NamedTemporaryFile(delete=True)
     f.write(b"")
     f.seek(0)
-    assert read_magic_number(f.name) is False
+    actual_result = read_magic_number(f.name)
     f.close()
+    assert actual_result is False
 
 
 def test_read_magic_number_returns_value_erorr():
