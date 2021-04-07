a5762ade41886a01bae00497a06d0348e595f3d2
mukhaw <mukhaw99@gmail.com>
2021-04-07 22:50:53 +0300

    Move doctests in function_body in homework 04 task 04

--- hm_1/tasks/task_04_04.py
+++ hm_1/tasks/task_04_04.py
@@ -35,4 +35,19 @@ def fizz_bass_element(number: int):
 
 
 def fizzbuzz(n: int) -> List[str]:
+    """
+
+    >>> fizzbuzz(5)
+    ['1', '2', 'fizz', '4', 'buzz']
+    >>> fizzbuzz(16)
+    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'FizzBuzz', '16']
+    >>> fizzbuzz(-1)
+    []
+    """
     return [fizz_bass_element(i) for i in range(1, n + 1)]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
