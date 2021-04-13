f2401368c2d347ed44ff628ce8298d1d2099ec49
mukhaw <mukhaw99@gmail.com>
2021-04-13 22:42:41 +0300

    Fix homework 04 task 02

--- hm_1/tasks/task_04_02.py
+++ hm_1/tasks/task_04_02.py
@@ -1,11 +1,34 @@
+"""
+Write a function that accepts an URL as input
+and count how many letters `i` are present in the HTML by this URL.
+Write a test that check that your function works.
+Test should use Mock instead of real network interactions.
+You can use urlopen* or any other network libraries.
+In case of any network error raise ValueError("Unreachable {url}).
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has positive and negative tests
+You will learn:
+ - how to test using mocks
+ - how to write complex mocks
+ - how to raise an exception form mocks
+ - do a simple network requests
+>>> count_dots_on_i("https://example.com/")
+59
+* https://docs.python.org/3/library/urllib.request.html#urllib.request.urlopen
+"""
 import urllib.request
-from collections import Counter
 
 
 def count_dots_on_i(url: str) -> int:
     try:
         with urllib.request.urlopen(url) as response:
-            html = str(response.read())
-        return Counter(html)["i"]
+            html = response.read()
+            count = 0
+            for i in str(html):
+                if i == "i":
+                    count += 1
+        return count
     except Exception:
         raise ValueError(f"Unreachable {url}")
