64ffdb060514ab37d74549d00424abc01d49a5ae
mukhaw <mukhaw99@gmail.com>
2021-04-13 20:04:22 +0300

    Fix homework 04 test 02 and task 02

--- hm_1/tasks/task_04_02.py
+++ hm_1/tasks/task_04_02.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 
 def count_dots_on_i(url: str) -> int:
-    with urllib.request.urlopen(url) as response:
-        html = str(response.read())
-    return Counter(html)["i"]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = str(response.read())
+        return Counter(html)["i"]
+    except Exception:
+        raise ValueError(f"Unreachable {url}")
--- test/test_04_02.py
+++ test/test_04_02.py
@@ -1,10 +1,23 @@
-from unittest.mock import patch
+from unittest.mock import Mock
+
+import pytest
 
 from hm_1.tasks.task_04_02 import count_dots_on_i
 
 
-def test_count_dots_on_i():
-    with patch("requests.get") as mock_request:
-        url = "https://docs.python.org/3/library/urllib.request.html#urllib.request.urlopen"
-        mock_request.return_value = 7019
-        assert count_dots_on_i(url) == mock_request.return_value
+@pytest.mark.parametrize(
+    ("url", "expected_result"),
+    [
+        ("https://example.com/", 59),
+        ("https://docs.python.org/3/howto/urllib2.html", 2052),
+    ],
+)
+def test_count_dots_on_i(url: str, expected_result: int):
+    mock = Mock(side_effect=count_dots_on_i)
+    assert mock(url) == expected_result
+
+
+def test_count_dots_on_i_return_value_error():
+    url = "https://not_exist_url.com/"
+    with pytest.raises(ValueError, match=f"Unreachable {url}"):
+        count_dots_on_i(url)
