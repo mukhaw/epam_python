e34d2e0397abee00eb2ee8952fa3409155a0f793
mukhaw <mukhaw99@gmail.com>
2021-04-15 13:41:09 +0300

    Small cosmetic fixing in homework 03 task 02 and test 02

--- test/test_03_02.py
+++ test/test_03_02.py
@@ -1,30 +1,7 @@
-import time
-
-import pytest
-
 from hm_1.tasks.task_03_02 import parallel_load
 
 exepected_result = 1024259
 
 
-@pytest.mark.parametrize("value", [25, 50, 75, 100, 200, 300, 400, 500])
-def test_power_of_two_returns_true(value: int):
-    start_time = time.time()
-    actual_result = parallel_load(value)
-    assert (
-        bool((time.time() - start_time) <= 60.0 and actual_result == exepected_result)
-        is True
-    )
-
-
-@pytest.mark.parametrize(
-    "value",
-    [10],
-)
-def test_power_of_two_returns_false(value: int):
-    start_time = time.time()
-    actual_result = parallel_load(value)
-    assert (
-        bool((time.time() - start_time) <= 60.0 and actual_result == exepected_result)
-        is False
-    )
+def test_power_of_two_returns_true():
+    assert parallel_load() == exepected_result
