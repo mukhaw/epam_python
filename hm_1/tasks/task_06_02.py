c9354d0dcdf88515a5572f5b56dc3213c2318e25
mukhaw <mukhaw99@gmail.com>
2021-04-21 15:23:47 +0300

    Add homework 06 task 02

---
+++ hm_1/tasks/task_06_02.py
@@ -0,0 +1,89 @@
+"""
+В этом задании будем улучшать нашу систему классов из задания прошлой лекции
+(Student, Teacher, Homework)
+Советую обратить внимание на defaultdict из модуля collection для
+использования как общую переменную
+1. Как то не правильно, что после do_homework мы возвращаем все тот же
+объект - будем возвращать какой-то результат работы (HomeworkResult)
+HomeworkResult принимает объект автора задания, принимает исходное задание
+и его решение в виде строки
+Атрибуты:
+    homework - для объекта Homework, если передан не этот класс -  выкинуть
+    подходящие по смыслу исключение с сообщением:
+    'You gave a not Homework object'
+    solution - хранит решение ДЗ как строку
+    author - хранит объект Student
+    created - c точной датой и временем создания
+
+
+    Вопросы:
+    1)Это должна быть функция или класс
+    2)если это функция анутри do_homework то нужен ли нам автор, логично
+    3) она должна быть отдельной
+    4)
+2. Если задание уже просрочено хотелось бы видеть исключение при do_homework,
+а не просто принт 'You are late'.
+Поднимайте исключение DeadlineError с сообщением 'You are late' вместо print.
+3. Student и Teacher имеют одинаковые по смыслу атрибуты
+(last_name, first_name) - избавиться от дублирования с помощью наследования
+4.
+Teacher
+Атрибут:
+    homework_done - структура с интерфейсом как в словаря, сюда поподают все
+    HomeworkResult после успешного прохождения check_homework
+    (нужно гаранитровать остутствие повторяющихся результатов по каждому
+    заданию), группировать по экземплярам Homework.
+    Общий для всех учителей. Вариант ипользования смотри в блоке if __main__...
+Методы:
+    check_homework - принимает экземпляр HomeworkResult и возвращает True если
+    ответ студента больше 5 символов, так же при успешной проверке добавить в
+    homework_done.
+    Если меньше 5 символов - никуда не добавлять и вернуть False.
+    reset_results - если передать экземпряр Homework - удаляет только
+    результаты этого задания из homework_done, если ничего не передавать,
+    то полностью обнулит homework_done.
+PEP8 соблюдать строго.
+Всем перечисленным выше атрибутам и методам классов сохранить названия.
+К названием остальных переменных, классов и тд. подходить ответственно -
+давать логичные подходящие имена.
+"""
+
+from datetime import datetime
+
+
+class Person:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+
+class Homework:
+    def __init__(self, text, dead_line, created):
+        self.text = text
+        self.dead_line = dead_line
+        self.created = created
+
+    def is_active(self) -> bool:
+        return datetime.now() < self.dead_line + self.created
+
+
+class Student(Person):
+    def do_homework(self, hw: Homework):
+        try:
+            return hw
+        except Exception:
+            raise Exception('DeadlineError:"You are late"')
+
+
+class Teacher(Person):
+    @staticmethod
+    def create_homework(text, dead_line, created) -> Homework:
+        return Homework(text, dead_line, created)
+
+
+class HomeworkResult:
+    def __init__(self, homework, solution, author, created):
+        self.homrwork = homework
+        self.solution = solution
+        self.author = author
+        self.created = created
