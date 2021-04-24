c5c217afac2d4da9c88324910daacb870cef6148
mukhaw <mukhaw99@gmail.com>
2021-04-25 01:13:04 +0300

    Add homework 07 task 03 and test 03

---
+++ hm_1/tasks/task_07_03.py
@@ -0,0 +1,28 @@
+"""
+Given a Tic-Tac-Toe 3x3 board (can be unfinished).
+Write a function that checks if the are some winners.
+If there is "x" winner, function should return "x wins!"
+If there is "o" winner, function should return "o wins!"
+If there is a draw, function should return "draw!"
+If board is unfinished, function should return "unfinished!"
+Example:
+    [[-, -, o],
+     [-, x, o],
+     [x, o, x]]
+    Return value should be "unfinished"
+    [[-, -, o],
+     [-, o, o],
+     [x, x, x]]
+     Return value should be "x wins!"
+"""
+import numpy as np
+
+
+def tic_tac_toe_checker(board) -> str:
+    diagonals = [np.fliplr(board).diagonal(), board.diagonal(), ["-", "-", "-"]]
+    for i, j, k in zip(board, board.transpose(), diagonals):
+        if ("".join(i) or "".join(j) or "".join(k)) == "xxx":
+            return "x wins"
+        if "".join(i) == "ooo" or "".join(j) == "ooo" or "".join(k) == "ooo":
+            return "o wins"
+    return "unfinished"
---
+++ test/test_07_03.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from hm_1.tasks.task_07_03 import tic_tac_toe_checker
+
+
+def test_tic_tac_toe_returns_unfinished():
+    board = np.array([["-", "-", "o"], ["-", "o", "o"], ["-", "x", "x"]])
+    assert tic_tac_toe_checker(board) == "unfinished"
+
+
+def test_tic_tac_toe_returns_o_wins():
+    board = np.array([["-", "-", "o"], ["-", "o", "o"], ["-", "x", "o"]])
+    assert tic_tac_toe_checker(board) == "o wins"
