3d787191525594826df30cece0c7cc6abf21e2e1
mukhaw <mukhaw99@gmail.com>
2021-04-25 01:46:21 +0300

    Fix homework 07 task 01 and test 01

--- hm_1/tasks/task_07_01.py
+++ hm_1/tasks/task_07_01.py
@@ -13,5 +13,5 @@ def find_occurrences(tree: dict, element: Any) -> int:
     text_without_punctuation = str(tree).translate(
         str.maketrans("", "", string.punctuation)
     )
-    list_of_dict_elements = text_without_punctuation.split()
-    return list_of_dict_elements.count(element)
+    words = text_without_punctuation.split()
+    return words.count(element)
