ea34393a94f8fef8268105f886431a312152917d
mukhaw <mukhaw99@gmail.com>
2021-04-29 22:13:15 +0300

    Fix something in homework 08 task 1 and test 1

---
+++ hm_1/tasks/task_02_01.py
@@ -0,0 +1,64 @@
+"""
+Given a file containing text.txt. Complete using only default collections:
+    1) Find 10 longest words consisting from largest amount of unique symbols
+    2) Find rarest symbol for document
+    3) Count every punctuation char
+    4) Count every non ascii char
+    5) Find most common non ascii char for document
+"""
+import string
+from collections import Counter
+from typing import List
+
+
+def get_text(file_path: str) -> str:
+    data = ""
+    with open(file_path, encoding="unicode_escape") as f:
+        for line in f:
+            data += line
+    return data
+
+
+def get_longest_diverse_words(file_path: str) -> List[str]:
+    data = {}
+    text_without_punctuation = get_text(file_path).translate(
+        str.maketrans("", "", string.punctuation)
+    )
+    words = text_without_punctuation.split()
+    for i in words:
+        data[i] = len(set(i.lower()))
+    result = sorted(data.items(), key=lambda x: x[1], reverse=True)
+    list_10_longest_words = []
+    for i in sorted(result[0:10], key=lambda x: len(x[0]), reverse=True):
+        list_10_longest_words.append(i[0])
+    return list_10_longest_words
+
+
+def get_rarest_char(file_path: str) -> str:
+    data = Counter(get_text(file_path).lower())
+    key, _ = min(data.items(), key=lambda x: x[1])
+    return key
+
+
+def count_punctuation_chars(file_path: str) -> int:
+    text = get_text(file_path)
+    text_without_punctuation = text.translate(str.maketrans("", "", string.punctuation))
+    return len(text) - len(text_without_punctuation)
+
+
+def count_non_ascii_chars(file_path: str) -> int:
+    text = get_text(file_path)
+    text_without_non_ascii_chars = text.encode("ascii", "ignore").decode()
+    return len(text) - len(text_without_non_ascii_chars)
+
+
+def get_most_common_non_ascii_char(file_path: str) -> str:
+    text = get_text(file_path)
+    text_without_non_ascii_chars = text.encode("ascii", "ignore").decode()
+    non_ascii_chars = ""
+    for i in text:
+        if i not in text_without_non_ascii_chars:
+            non_ascii_chars += i
+    data = Counter(non_ascii_chars)
+    key, _ = max(data.items(), key=lambda x: x[1])
+    return key
---
+++ hm_1/tasks/task_05_01.py
@@ -0,0 +1,68 @@
+"""
+Необходимо создать 3 класса и взаимосвязь между ними (Student, Teacher,
+Homework)
+Наследование в этой задаче использовать не нужно.
+Для работы с временем использовать модуль datetime
+1. Homework принимает на вход 2 атрибута: текст задания и количество дней
+на это задание
+Атрибуты:
+    text.txt - текст задания
+    deadline - хранит объект datetime.timedelta с количеством
+    дней на выполнение
+    created - c точной датой и временем создания
+Методы:
+    is_active - проверяет не истекло ли время на выполнение задания,
+    возвращает boolean
+2. Student
+Атрибуты:
+    last_name
+    first_name
+Методы:
+    do_homework - принимает объект Homework и возвращает его же,
+    если задание уже просрочено, то печатет 'You are late' и возвращает None
+3. Teacher
+Атрибуты:
+     last_name
+     first_name
+Методы:
+    create_homework - текст задания и количество дней на это задание,
+    возвращает экземпляр Homework
+    Обратите внимание, что для работы этого метода не требуется сам объект.
+PEP8 соблюдать строго.
+Всем перечисленным выше атрибутам и методам классов сохранить названия.
+К названием остальных переменных, классов и тд. подходить ответственно -
+давать логичные подходящие имена.
+"""
+from datetime import datetime
+
+
+class Homework:
+    def __init__(self, text, dead_line, created):
+        self.text = text
+        self.dead_line = dead_line
+        self.created = created
+
+    def is_active(self) -> bool:
+        return datetime.now() < self.dead_line + self.created
+
+
+class Student:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def do_homework(self, hw: Homework):
+        if not hw.is_active():
+            print("You're late")
+            return None
+        return hw
+
+
+class Teacher:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    @staticmethod
+    def create_homework(text, dead_line, created) -> Homework:
+        return Homework(text, dead_line, created)
